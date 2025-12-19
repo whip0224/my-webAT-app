ef5f9db20b11e7589c75f04c3fcbc60a688225ea
whip0224 <80029079+whip0224@users.noreply.github.com>
2025-12-19 13:36:47 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,5 @@
-// sw.js
-self.addEventListener('install', (event) => {
-    console.log('Service Worker: Installed');
-});
-
-self.addEventListener('activate', (event) => {
-    console.log('Service Worker: Activated');
-});
-
-// 這是最關鍵的一段，必須有 fetch 事件
-self.addEventListener('fetch', (event) => {
-    // 這裡可以暫時不寫邏輯，但事件必須存在
+// sw.js 必須包含 fetch 事件，瀏覽器才會判定為 PWA
+self.addEventListener('fetch', function(event) {
+    // 目前為空，僅通過 PWA 安裝檢測
+    // 若需要離線瀏覽功能，需在此處加入 Cache 邏輯
 });
